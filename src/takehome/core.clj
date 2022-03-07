26cf5d7e2aff3bd69b6acdf74c69a4e3d67bef6e
JP Ventura <jpventura@users.noreply.github.com>
2022-03-07 16:09:29 -0300

    refactor: delete takehome project

--- src/takehome/core.clj
+++
@@ -1,9 +0,0 @@
-(ns takehome.core
-  (:require [java-time :as time]))
-
-(defn can-access? [object purchase]
-  (if (= (:type object) :movie)
-    (and (= (:type purchase) :patriota)
-         (time/before? (:subscription-start purchase)
-                       (:released-at object)
-                       (:subscription-end purchase)))))
