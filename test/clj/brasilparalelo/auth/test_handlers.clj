87c4247547d644cad9534d507e5914227600673f
JP Ventura <jpventura@users.noreply.github.com>
2022-03-07 15:45:58 -0300

    test(media): handlers unit tests

---
+++ test/clj/brasilparalelo/auth/test_handlers.clj
@@ -0,0 +1,6 @@
+(ns brasilparalelo.auth.handlers-test
+  (:require [clojure.test :refer :all])
+  (:require [brasilparalelo.auth.handlers :refer [refresh-token]]))
+
+(deftest refresh-token-test
+  )
