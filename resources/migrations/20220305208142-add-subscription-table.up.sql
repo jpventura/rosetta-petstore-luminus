46e313ce01f55cfa0faabc8d58d5f9aab891624c
JP Ventura <jpventura@users.noreply.github.com>
2022-03-25 16:26:41 -0300

    feat(auth): luminus mariadb migrations

---
+++ resources/migrations/20220305208142-add-subscription-table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE subscriptions
+(
+    id         UUID PRIMARY KEY,
+    user_id    UUID,
+    active     BOOLEAN,
+    type       VARCHAR(16),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    CONSTRAINT subscriptions_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
