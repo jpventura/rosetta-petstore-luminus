6428fe607f705211596f38fa72ce513aeb3b18ae
JP Ventura <jpventura@users.noreply.github.com>
2022-04-01 21:23:42 -0300

    build(users): docker mariadb content samples

--- .local/etc/mariadb/docker-entrypoint-initdb.d/20220307202151-insert-users-into-table.sql
+++ .local/etc/mariadb/docker-entrypoint-initdb.d/20220307202151-insert-users-into-table.sql
@@ -4,4 +4,8 @@ VALUES
     ('0386a23b-da8d-4654-9dbd-9bbe2bb2e66c', 'Miles Bennett', 'Dyson', 'mdyson@cyberdyne.com', '1', '1991-07-01T00:00:00', '1997-08-29T00:00:00', '1', 'aiH0Diuchik2Quoo', ''),
     ('26bd2f1f-98a5-4c65-9baf-347fa43f5194', 'Sarah', 'Connor', 'sconnor@aol.com', '0', '1991-07-01T00:00:00', '1997-08-29T00:00:00', '1', 'Iru0Dahng3Eepiel', ''),
     ('4617143c-a561-4c15-b911-2f72bfd24bb1', 'John', 'Connor', 'jconnor@yahoo.com', '0', '1991-07-01T00:00:00', '1997-08-29T00:00:00', '1', 'Eez3xahfaero9eih', ''),
-    ('fc84305d-96b5-4e78-a28e-acf1d7e70a06', 'Kyle', 'Reese', 'reese@skynet.com', '0', '1991-07-01T00:00:00', '1997-08-29T00:00:00', '1', 'Zahjo6joh4Gah8bu', '');
+    ('fc84305d-96b5-4e78-a28e-acf1d7e70a06', 'Kyle', 'Reese', 'reese@skynet.com', '0', '1991-07-01T00:00:00', '1997-08-29T00:00:00', '1', 'Zahjo6joh4Gah8bu', ''),
+    ('c3fb393d-49ba-4ab0-8880-d93dade41fa2', 'Alex', 'Murphy', 'amurphy@dpd.gov', '0', '1991-07-01T00:00:00', '1997-08-29T00:00:00', '1', 'aiH0Diuchik2Quoo', ''),
+    ('8689705b-2ea3-4e2c-a639-00e9e73d6cc7', 'Anne', 'Lewis', 'alewis@dpd.gov', '0', '1991-07-01T00:00:00', '1997-08-29T00:00:00', '1', 'Iru0Dahng3Eepiel', ''),
+    ('ef03897c-77b0-4cfe-be12-55d14a213cdf', 'Bob', 'Morton', 'bmorton@ocp.com', '1', '1991-07-01T00:00:00', '1997-08-29T00:00:00', '1', 'Eez3xahfaero9eih', ''),
+    ('7b548648-6931-4823-b0f5-54661766ca95', 'Dick', 'Jones', 'djones@ocp.com', '1', '1991-07-01T00:00:00', '1997-08-29T00:00:00', '1', 'Zahjo6joh4Gah8bu', '');
